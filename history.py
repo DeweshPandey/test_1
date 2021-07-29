84760908e6fbcf63511e77563139a519ffb2cf00
Dewesh Pandey <p.dewesh@iitg.ac.in>
2021-07-29 13:38:04 +0530

    init commit

---
+++ history.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat May 22 17:11:37 2021)---
+runfile('C:/Users/dewes/.spyder-py3/temp.py', wdir='C:/Users/dewes/.spyder-py3')
+
+## ---(Sat May 29 20:44:29 2021)---
+runfile('C:/Users/dewes/.spyder-py3/temp.py', wdir='C:/Users/dewes/.spyder-py3')
+
+## ---(Mon May 31 18:49:11 2021)---
+runfile('C:/Users/dewes/.spyder-py3/temp.py', wdir='C:/Users/dewes/.spyder-py3')
+
+## ---(Wed Jun  9 14:30:23 2021)---
+runfile('C:/Users/dewes/untitled0.py', wdir='C:/Users/dewes')
+runcell(0, 'C:/Users/dewes/untitled0.py')
+runfile('C:/Users/dewes/untitled0.py', wdir='C:/Users/dewes')
+runcell(0, 'C:/Users/dewes/untitled0.py')
+runfile('C:/Users/dewes/untitled0.py', wdir='C:/Users/dewes')
+
+## ---(Sat Jul 17 14:16:49 2021)---
+runfile('E:/Rootics/ope cv/color detection.py', wdir='E:/Rootics/ope cv')
+
+## ---(Sat Jul 17 14:39:55 2021)---
+runcell(0, 'E:/Rootics/ope cv/shapedetection.py')
+runfile('E:/Rootics/ope cv/shapedetection.py', wdir='E:/Rootics/ope cv')
+runcell(0, 'E:/Rootics/ope cv/shapedetection.py')
+runfile('E:/Rootics/ope cv/shapedetection.py', wdir='E:/Rootics/ope cv')
+runcell(0, 'E:/Rootics/ope cv/shapedetection.py')
+runfile('E:/Rootics/ope cv/shapedetection.py', wdir='E:/Rootics/ope cv')
+runcell(0, 'E:/Rootics/ope cv/shapedetection.py')
